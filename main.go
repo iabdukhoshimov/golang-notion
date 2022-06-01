e9205c2345f8fe3331de4d5c0c78ce49526905e6
an-ikhtiyour <an.ikhtiyour@gmail.com>
2022-06-01 16:47:10 +0500

    slices:added

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
-import "fmt"
-
 func main() {
-	Hello_world()
-	Value()
-	Constat()
-	Iteration()
-	Control()
-	Switches()
-	Array()
-	Lifo()
-	fmt.Println(Swap())
+	// Hello_world()
 
-	x := []int{1, 2, 3, 4, 5}
-	Reverse_Slice(x)
-	fmt.Println(x)
+	// Value()
 
-	Check()
+	// Constat()
+
+	// Iteration()
+
+	// Control()
+
+	// Switches()
+
+	// Array()
+
+	// Lifo()
+
+	// fmt.Println(Swap())
+
+	// x := []int{1, 2, 3, 4, 5}
+	// Reverse_Slice(x)
+	// fmt.Println(x)
+
+	// Check()
+
+	Slices()
+
 }
