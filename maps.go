8b9cd6e1b89ec0e5c2d890a475dbc0e8b30d1d1b
an-ikhtiyour <an.ikhtiyour@gmail.com>
2022-06-02 08:42:42 +0500

    maps added

--- maps.go
+++ maps.go
@@ -9,4 +9,18 @@ func Maps() {
 	m["k2"] = 4
 
 	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1 ", v1)
+
+	fmt.Println("len: ", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
 }
